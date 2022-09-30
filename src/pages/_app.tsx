9cac0ef86b0d4da1133b6b58d3bbb03d768cb2d6
Fabrizio Lamanna <fabriziolamanna18@gmail.com>
2022-09-30 19:47:21 +0200

    adding google things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,24 @@ import { AppProps } from 'next/app';
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6B6PTGR2SL"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6B6PTGR2SL');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 );
 
 export default MyApp;
