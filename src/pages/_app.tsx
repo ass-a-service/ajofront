8a0366d09291328519f40dfe970a099d4f3846e6
Fabrizio Lamanna <fabriziolamanna18@gmail.com>
2022-09-30 20:16:36 +0200

    nope

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,9 @@
 import { AppProps } from 'next/app';
-import Script from 'next/script';
 
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-6B6PTGR2SL"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-6B6PTGR2SL');
-        `}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  );
+  <Component {...pageProps} />
 );
 
 export default MyApp;
